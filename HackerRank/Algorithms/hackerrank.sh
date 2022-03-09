f3a32a7e58879827565ece74c82e996953f433a0
Elfiend <elfiend@gmail.com>
2022-03-10 00:50:40 +0800

    Dynamic load project target.

--- HackerRank/Algorithms/hackerrank.sh
+++ HackerRank/Algorithms/hackerrank.sh
@@ -1,31 +1,61 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-function executeTest()
-{
+function executeTest() {
 	project=$1
-	if [ ! -e ${project}.py ]
-	then
+	if [ ! -e ${project}.py ]; then
 		echo "$project is not exist."
 		return
 	fi
 
-	python3 ${project}.py < ${project}.txt
+	echo "${project} Excutes."
+	python3 ${project}.py <${project}.txt
+}
+
+function chooseDifficulty() {
+	PS3="Choose the test Difficulty: "
+	ProjectsDirs=(
+		"exit"
+		$(ls -d */)
+	)
+	select projectDir in ${ProjectsDirs[@]}; do
+
+		if [ "$projectDir" == "exit" ]; then
+			exit 0
+		fi
+		if [ ! -d "$projectDir" ]; then
+			echo "$projectDir is not a folder."
+			continue
+		fi
+
+		break
+	done
+}
+
+function chooseProject() {
+	PS3="Choose the test project: "
+	projects=(
+		"exit"
+		".."
+		$(ls -t ${projectDir}*.py | sed 's/\.py//g')
+	)
+	select project in ${projects[@]}; do
+		case $project in
+		exit)
+			exit 0
+			;;
+		..)
+			break
+			;;
+		*)
+			executeTest ${project}
+			;;
+		esac
+	done
 }
 
 export OUTPUT_PATH=$(pwd)/output.txt
-PS3="Choose the test target: "
-Projects=(
-	"exit"
-)
 
-select project in ${Projects[@]}
-do
-	case $project in
-	exit)
-		break
-	;;
-	*)
-		executeTest ${project}
-	;;
-	esac
+while :; do
+	chooseDifficulty
+	chooseProject
 done
